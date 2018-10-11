ee1274746767e2dab7b114d1e0b6e6737bf3bcbc
Migacz85 <migacz85@gmail.com>
2018-10-11 14:19:17 +0100

    create: basic template with bootstrap and generate links using flask

--- run.py
+++ run.py
@@ -1,12 +1,20 @@
 import os
-from flask import ( Flask)
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "<h1>Hello There!</h1>"
+    return render_template("home.html", active_page='index')
+
+@app.route('/about')
+def about():
+    return render_template("about.html", active_page='about')
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html", active_page='contact')
 
 if __name__ == '__main__':
    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
